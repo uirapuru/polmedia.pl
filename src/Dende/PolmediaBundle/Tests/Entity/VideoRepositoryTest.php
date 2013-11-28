75372d2c3de75fe464953a0ea0ab721e9ac7b854
Grzegorz Kaszuba <uirapuru@tlen.pl>
2013-11-28 23:09:01 +0100

    config and tests

---
+++ src/Dende/PolmediaBundle/Tests/Entity/VideoRepositoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dende\PolmediaBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class VideoRepositoryTest extends WebTestCase
+{
+    public function getPromotedIndex()
+    {
+
+    }
+}
